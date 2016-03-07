b6a52af2dcf96d18fd0a6bdfea9ef8fbb7c7e687
tie219 <tie219@lehigh.edu>
2016-03-07 03:31:40 +0000

    updated hw06

--- hw06/Fibonacci.java
+++ hw06/Fibonacci.java
@@ -13,62 +13,93 @@ public class Fibonacci {
       Scanner input = new Scanner(System.in);
       //Construct Scanner
       int a1 = 0;
+      //Declare and initialize input value
       int a2 = 0;
+      //Declare and initialize input value
       int n = 0;
+      //Declare and initialize input value for number of digits
       int nextNum = 0;
+      //Next number in seuqnce
       int i = 0;
+      //Counter
       String wrong  = "";
+      //String for invalid input
       boolean condition = false;
+      //Condition for loop
       //Initialize and declare values for user input
       while (condition == false){
           System.out.println("Please enter a positive integer:");
+          //First input
            while (!input.hasNextInt()){
+               //Not a valid integer
                wrong = input.next();
+               //Stores string
                System.out.println("Invalid input, try again:");
            }
            a1 = input.nextInt();
+           //Stores correct input
            while (a1<0){
+               //Values less than zero
                System.out.println("Invalid input, try again:");
                a1 = input.nextInt();
+               //Stores value
            }
            
            System.out.println("Please enter a second positive integer:");
+           //Input of a second integer
            while (!input.hasNextInt()){
+               //Not a valid integer
                wrong = input.next();
+               //Stores incorrect
                System.out.println("Invalid input, try again:");
            }
            a2 = input.nextInt();
+           //Stores correct input
            while (a2<0){
+               //Integer values less than zero
                System.out.println("Invalid input, try again:");
                a2 = input.nextInt();
+               
            }
            
            System.out.println("How many custom Fibonnaci numbers should be printed?");
            while (!input.hasNextInt()){
+               //Incorrect input
                wrong = input.next();
+               //Stores incorrect
                System.out.println("Invalid input, try again:");
            }
            n = input.nextInt();
+           //Number of digits
            while (n<0){
+               //Cant be less than 0
                System.out.println("Invalid input, try again:");
                n = input.nextInt();
            }
            condition = true;
+           //Breaks loop when conditions are fulfilled
       }
  System.out.println("The numbers are:");
       while (i<n-3){
+          //Times to run through loop with counter
           while (i<1){
+              //User's input numbers
                System.out.print(a1 + "," + a2 + ",");
                break;
           }
         nextNum = a1+a2;
+        //Next number in sequence
         System.out.print(nextNum + ",");
-        
+        //Prints next number with comma
         a1 = a2;
+        //Stores second number into first
         a2 = nextNum;
+        //Stores second number into the next
          i++;
+         //Increments counter
       }
       System.out.print(nextNum + a1 + ".");
+      //Last number in sequence to end with period
       System.out.println("\n");
       
       
--- hw06/RunFactorial.java
+++ hw06/RunFactorial.java
@@ -12,25 +12,49 @@ public class RunFactorial {
   public static void main(String[] args) {
      Scanner input = new Scanner(System.in);
       //Construct Scanner
-      int num = 10;
+      int num = 0;
+      //Initialize and declare input
       String error = "";
+      //Error variable
       boolean condition = false;
+      //Condition for while loop
       while (condition == false){
           System.out.println("Please enter a positive integer:");
-          error = input.next();
-           while (error>9 && error <16 ){
-            num = input.nextInt();
-              break;
-           }
-       while (error>16 || error<9 || !input.hasNextInt()){
+          //User inputs number
+               while (!input.hasNextInt()){
+                   //If input is not an integer
+                error = input.next();
+                //Error saved as string
                System.out.println("Invalid input, try again:");
-               error = input.next();
+       }
+       num = input.nextInt();
+       //Integer stored
+       while (num<9 || num>16){
+           //Conditions on integer between 9 and 16
+           System.out.println("Invalid input, try again:"); 
+           while (!input.hasNextInt()){
+               //If input is still not an integer
+                error = input.next();
+               System.out.println("Invalid input, try again:");
            }
-               num = input.nextInt();
-
+           num = input.nextInt();
+           //Stores final correct value
+       }
         condition = true;
+        //Loop breaks on change in boolean value
       }
-      System.out.println(num);
-  }
-  }
       
+      int i = 1;
+      //Counter
+      int fact = 1;
+      //Starting factorial value
+      while (i<num){
+        fact = fact * i;
+        //Factorial increases with counter until input reached
+        i++;
+      }
+      System.out.println("Input accepted:");
+      System.out.println(num+"!"+"="+fact);
+      //Value printed
+  }
+  }
